a5b3de8ebd46c7e0110694b121656f830acc9ac0
sabarish-vm <sabarish.science@gmail.com>
2025-09-01 16:30:23 +0200

    neutral_standard_bondi problem push

---
+++ run_bondi_neutral.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# run a Bondi setup simulation in the folder given below, with the number of
+# threads set below
+
+folder=build_bondi_neutral
+nthread=8
+
+cmake_command=$(python write_configuration_bondi_neutral.py)
+
+mkdir $folder
+cd $folder
+echo $cmake_command
+eval $cmake_command
+make -j $nthread
+OMP_NUM_THREADS=$nthread OMP_PROC_BIND=True ./HydroCodeSpherical1D 2>&1 |
+    tee bondi_setup.log
+cd ..
